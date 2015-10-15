d9699502ce80973feb1910ca12106d2cbdbcea51
RhettDelFierro <thinquetank@gmail.com>
2015-10-14 19:07:04 -0700

    js_functions

---
+++ js_functions/main.js
@@ -0,0 +1,27 @@
+function myMessage() {
+    console.log('message');
+}
+
+function add(x,y) {
+    var z = x + y;
+    console.log(z);
+}
+
+function add2 (x,y) {
+    var z = x + y;
+    return z;
+}
+
+var add2result = add2(2142134,32412214);
+
+function cardFlip(clickedElement) {
+    $(clickedElement).hide();
+    src = getSrc(clickedElement);
+    console.log(src);
+}
+
+function getSrc(clickElement) {
+    return $(clickElement).parent().find('.class').attr('src');
+}
+
+var result = add2(add2(5,10), add2(20,30));
