cce27286bffbbebd75e9dfca0ed23ae6813ced6d
Christopher Eaton <christopherpeaton@gmail.com>
2015-11-16 18:04:51 -0800

    feature set 2 done

--- js_functions/main.js
+++ js_functions/main.js
@@ -1,9 +1,13 @@
-function myMessage(){
+function myMessage() {
     console.log("i heart functions");
 }
 
-function add(x,y){
-    x = 5;
-    y = 4;
+function add(x, y) {
     console.log(x + y);
 }
+
+function add2(x, y) {
+    var total = x + y;
+    return total;
+}
+var add2result = add2(10, 36);
