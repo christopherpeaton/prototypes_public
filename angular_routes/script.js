c04ad62a0205cb238ef8019db967db6da5a384fa
Christopher Eaton <christopherpeaton@gmail.com>
2015-12-14 15:12:15 -0800

    feature set 1 done

--- angular_routes/script.js
+++ angular_routes/script.js
@@ -1,22 +1,45 @@
 // Create the route module and name it routeApp
-
+var routeApp = angular.module("routeApp", ['ngRoute']);
 // Config the routes
+routeApp.config(function($routeProvider){
 
+    $routeProvider
          // route for the home page
-
+        .when('/', {
+            templateUrl: "pages/home.html",
+            controller: "mainCtrl"
+        })
         // route for the about page
-
+        .when('/about', {
+            templateUrl: "pages/about.html",
+            controller: "aboutCtrl"
+        })
         // route for the contact page
-
-
+        .when('/contact', {
+            templateUrl:"pages/contact.html",
+            controller: "contactCtrl"
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
 // Create the controllers for the different pages below
+routeApp.controller("routeCtrl", function($scope){
+    $scope.message = "lorem ipsum";
+});
 
 // home page controller
     // Create a message to display in the view
-
-
+routeApp.controller("mainCtrl", function($scope){
+    $scope.message = "lorem ipsum";
+});
 // about page controller
     // Create a message to display in the view
-
+routeApp.controller("aboutCtrl", function($scope){
+    $scope.message = "lorem ipsum";
+});
 // contact page controller
     // Create a message to display in the view
+routeApp.controller("pagesCtrl", function($scope){
+    $scope.message = "lorem ipsum";
+});
